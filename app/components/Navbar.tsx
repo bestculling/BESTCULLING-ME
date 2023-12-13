acbdf6eab564e52d818b4f2aa81bd999c83c7bf6
bestculling <aukkrapolarm@gmail.com>
2023-12-13 08:54:49 +0700

    Initail Project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Socials } from "../constants";
+import Image from "next/image";
+
+function Navbar() {
+  return (
+    <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between px-10 md:px-20">
+      <div className="flex flex-row gap-3 items-center">
+        <div className="relative">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={45}
+            height={45}
+            className="w-full h-full object-contain bg-gradient-to-r from-purple-500 to-cyan-500 rounded-full"
+          />
+        </div>
+        <h1 className="text-white text-[25px] font-semibold">
+          BESTCULLING{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+            Dev
+          </span>
+        </h1>
+      </div>
+      <div className="flex flex-row gap-5 mb-2">
+        {Socials.map((social) => (
+          <Image
+            key={social.name}
+            src={social.src}
+            alt={social.name}
+            width={28}
+            height={28}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
